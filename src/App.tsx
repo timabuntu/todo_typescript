0fb5df03bd9c1f08be44b2c912a7a34042eefc46
thiago lopes de mello <timabuntu@gmail.com>
2022-01-31 11:28:48 -0300

    feat: created styled header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-function App() {
+import Header from "./components/Header";
+
+const App: React.FC = () => {
   return (
-  <div className="text-3xl font-bold">
-    <h1>Todo</h1>
-  </div>
-  )
-}
+    <div className="min-h-screen bg-slate-50">
+      <div className="flex items-center justify-center h-48 bg-blue-400">
+        <Header />
+      </div>
+      <div className="container mx-auto bg-gray-300">
+        <h1>main</h1>
+      </div>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+const Header: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-semibold text-slate-100">
+        to<span>.Do</span>
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
